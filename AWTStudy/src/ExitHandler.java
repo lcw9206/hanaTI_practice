05d8b87694bf4caa26b0fb34d94b415207ab66d8
Lee <lcw9206@gmail.com>
2018-08-31 09:23:05 +0900

    Adapter 클래스 이용예제

--- AWTStudy/src/ExitHandler.java
+++ AWTStudy/src/ExitHandler.java
@@ -1,19 +1,15 @@
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class ExitHandler implements WindowListener {
+// 어댑터 클래스를 이용하면 이벤트 처리에 필요없는 메서드를 상속받지 않아도 된다.
+public class ExitHandler extends WindowAdapter {
 
 	// 참조받기 위한 frame 선언 (생성자 or getter/setter로 선언 가능)
 	ChatFrame frame;
 	
 	public ExitHandler(ChatFrame frame) {
 		this.frame = frame;
-	}
-	
-	@Override
-	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -23,34 +19,5 @@ public class ExitHandler implements WindowListener {
 
 	}
 
-	@Override
-	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
