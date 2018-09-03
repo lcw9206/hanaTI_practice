b0015561d7c283c7a8ed7e23dac19e0d97b5cb8d
Lee <lcw9206@gmail.com>
2018-09-03 20:02:10 +0900

    BufferFilter 예제

---
+++ IOStudy/src/BufferedInputStreamExample.java
@@ -0,0 +1,33 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class BufferedInputStreamExample {
+	static final String path = "/Users/mcbookpro/Downloads/수강확인증_이철우_180824.pdf";
+	
+	public static void main(String[] args) throws IOException {
+		// Node Stream
+		InputStream fin = null;
+		// 물을 땡겨온다.
+		fin = new FileInputStream(path);
+		
+		// Filter Stream
+		BufferedInputStream in = null;
+		// 땡겨온 물을 사용한다. 데이터 임의 접근이 가능하다.
+		in = new BufferedInputStream(fin); // default = 512바이트 배열 
+		in.read();
+		System.out.println(in.read());
+		in.read();
+		System.out.println(in.read());
+		in.read();
+		System.out.println(in.read());
+		in.read();
+		System.out.println(in.read());
+		in.mark(2); // read로 데이터를 읽다가 reset 메서드를 이용해 mark한 곳으로 이동할 수 있다.
+		in.skip(20); // n바이트만큼 건너뛴다.
+		in.reset(); // mark한 곳으로 되돌아간다.
+		System.out.println(in.read());
+	}
+}
