ad1c3d6ac5c174c4161bfee717eb6cc5e52d20f0
Lee <lcw9206@gmail.com>
2018-09-06 14:13:06 +0900

    Thread 클래스를 이용한 멀티스레드 예제

--- ThreadStudy/src/Car.java
+++ ThreadStudy/src/Car.java
@@ -1,11 +1,27 @@
-public class Car {
-	String name;
+import java.util.Random;
 
+public class Car extends Thread {
+	// 쓰레드 클래스에는 name 변수가 이미 존재한다.
 	public Car(String name) {
-		this.name = name;
+		// super(name);
+		setName(name);
 	}
 
+	// 쓰레드의 엔트리 포인트 역할
 	public void run() {
-		System.out.println("가즈아!");
+		Random random = new Random();
+		System.out.println(getName() + "자동차 가즈아!");
+		for (int i = 0; i <= 100; i++) {
+		
+		// 각각의 쓰레드이므로 예외처리에서 throws 불가능	
+		try {
+			Thread.sleep(random.nextInt(500));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			System.out.println(getName() + " 자동차 " + i + "미터 전진");
+		}
+		System.out.println(getName() + "자동차 Finish");
 	}
 }
--- ThreadStudy/src/CarExample.java
+++ ThreadStudy/src/CarExample.java
@@ -1,11 +1,20 @@
 
 public class CarExample {
 
+	// 메인 쓰레드의 엔트리 포인트 역할
 	public static void main(String[] args) {
 		System.out.println("메인 스레드에 의해 프로그램 시작됨");
 
 		Car car = new Car("방그리");
-		car.run();
+		// car.run(); => Thread를 상속받아도 직접 호출하면 main 쓰레드.
+		// 이에 독립 쓰레드를 생성하기 위해 start 메서드 이용
+		car.start();
+		
+		Car car2 = new Car("홍길동");
+		car2.start();
+		
+		Car car3 = new Car("리그방");
+		car3.start();
 		
 		System.out.println("메인 스레드에 의해 프로그램 종료됨");
 	}
