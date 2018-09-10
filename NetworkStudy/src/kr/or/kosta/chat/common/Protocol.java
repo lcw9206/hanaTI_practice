cccf0459ba71ea541877cbff9f36f5bbaa049b7a
Lee <lcw9206@gmail.com>
2018-09-10 15:49:52 +0900

    네트워크 과제 초기버전(접속, 중복처리)

---
+++ NetworkStudy/src/kr/or/kosta/chat/common/Protocol.java
@@ -0,0 +1,12 @@
+package kr.or.kosta.chat.common;kr
+
+public interface Protocol {
+
+	// 프로토콜이 늘어날 경우 데이터 흐름을 해쉬태그처럼 붙여주면 좋다.
+	public static final String DELEMETER = ",";
+	
+	public static final int CONNECT = 1000;
+	public static final int CONNECT_RESULT = 1001;
+	public static final int MULTI_CHAT = 2000;
+	public static final int DISCONNECT = 3000;
+}
